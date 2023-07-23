97c3418419d3eb8eb65c2558652891a19a89a1b0
Akira Suzuki <a28195636@gmail.com>
2023-07-23 11:40:24 +0000

    Ruby on Rails 06

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  # PostsController
+  get 'posts/new', to: 'posts#new', as: 'new_post'
+  get 'posts/edit', to: 'posts#edit', as: 'edit_post'
+  
 end
