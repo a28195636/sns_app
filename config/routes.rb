9680c3319b19f6e05ae2e23114488d18591f6978
Akira Suzuki <a28195636@gmail.com>
2023-07-30 11:49:07 +0000

    Ruby on Rails 09

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
+
   # PostsController
+  get '/', to: 'posts#index', as: 'index_post'
   get 'posts/new', to: 'posts#new', as: 'new_post'
-  get 'posts/edit', to: 'posts#edit', as: 'edit_post'
-  get 'posts/', to: 'posts#index', as: 'index_post'
+  # この行を追加
+  post 'posts/new', to: 'posts#create', as: 'create_post'
+
+  # TopicsController
+  get 'topics/new', to: 'topics#new', as: 'new_topic'
+  get 'topics/edit/:id', to: 'topics#edit', as: 'edit_topics'
+  # 課題３で追加
+  post 'topics/new', to: 'topics#create', as: 'create_topics'
+  # 課題６で追加
+  post 'topics/edit', to: 'topics#update', as: 'update_topics'
   
 end
